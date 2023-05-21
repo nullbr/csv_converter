ec28aa1e86b4e9836c73ca169ab5b596ac4b68e7
Bruno Leite <bmarianoleite3@gmail.com>
2023-05-21 17:59:54 -0300

    adding home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'home#index'
+
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+
   api_version(module: 'V1', header: { name: 'Accept-Version', value: '1' }, default: true) do
     post '/convert', to: 'converters#convert'
   end
